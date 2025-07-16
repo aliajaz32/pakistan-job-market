8f2ee00dfb91934ab5b50f4d2e52d7fc66d13e0a
aliajaz32 <aliajaz32.aa@gmail.com>
2025-07-16 13:01:46 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,65 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, onValue } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBOWAOVnS7-1DFHZ_OwUYacQBe3TLwe-mw",
+  authDomain: "pakistan-job-market.firebaseapp.com",
+  projectId: "pakistan-job-market",
+  storageBucket: "pakistan-job-market.appspot.com",
+  messagingSenderId: "361603670702",
+  appId: "1:361603670702:web:57f3ddc394f6c611c4833f",
+  measurementId: "G-325LXBT719"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase();
+
+
+const db = getDatabase();
+const starCountRef = ref(db, 'posts/' + postId + '/starCount');
+onValue(starCountRef, (snapshot) => {
+  const data = snapshot.val();
+  updateStarCount(postElement, data);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
